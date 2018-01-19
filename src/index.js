9c78805aad067430540d5a5423389e909b2e0339
Andela-Didacus <didacus.abongo@andela.com>
2018-01-19 15:38:47 +0300

    feat(frozen-moments): create initial project setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/, loader: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
+
+module.exports = config;
